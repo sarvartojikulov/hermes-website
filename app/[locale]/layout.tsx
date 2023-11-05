8258f3c53eb02731765a95d14ab87b6517bcbd49
Sarvar Tojikulov <sarvar3099@gmail.de>
2023-11-05 16:09:58 +0100

    feat: ssg

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,60 +6,33 @@ import "./globals.css";
 
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
+import { ReactNode } from "react";
 
 export function generateStaticParams() {
   return [{ locale: "en" }, { locale: "ua" }];
 }
 
+type Props = {
+  children: ReactNode;
+  params: { locale: string };
+};
+
 const inter = Wix_Madefor_Display({ subsets: ["latin", "cyrillic"] });
 
 export const metadata: Metadata = {
-  title: "Hemes",
+  title: "Hermes",
   description: "Hermes charity fund",
 };
 
 export default async function RootLayout({
   children,
   params: { locale },
-}: {
-  children: React.ReactNode;
-  params: any;
-}) {
-  let home;
-  let donate;
-  let contact;
-  let privacyPolicy;
-  let commons;
-  let news;
-  try {
-    home = (await import(`../../i18n/pages/home/${locale}.json`)).default;
-    privacyPolicy = (
-      await import(`../../i18n/pages/privacy-policy/${locale}.json`)
-    ).default;
-    commons = (await import(`../../i18n/pages/common/${locale}.json`)).default;
-
-    news = (await import(`../../i18n/pages/news/${locale}.json`)).default;
-
-    donate = (await import(`../../i18n/pages/donate/${locale}.json`)).default;
-    contact = (await import(`../../i18n/pages/contact/${locale}.json`)).default;
-  } catch (error) {
-    notFound();
-  }
-
+}: Props) {
+  const messages = await getIntlMessages(locale);
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextIntlClientProvider
-          locale={locale}
-          messages={{
-            ...home,
-            ...privacyPolicy,
-            ...commons,
-            ...news,
-            ...donate,
-            ...contact
-          }}
-        >
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <Navigation />
           {children}
           <Footer />
@@ -67,4 +40,33 @@ export default async function RootLayout({
       </body>
     </html>
   );
+}
+
+async function getIntlMessages(locale: string) {
+  try {
+    const home = (await import(`../../i18n/pages/home/${locale}.json`)).default;
+    const privacyPolicy = (
+      await import(`../../i18n/pages/privacy-policy/${locale}.json`)
+    ).default;
+    const commons = (await import(`../../i18n/pages/common/${locale}.json`))
+      .default;
+
+    const news = (await import(`../../i18n/pages/news/${locale}.json`)).default;
+
+    const donate = (await import(`../../i18n/pages/donate/${locale}.json`))
+      .default;
+    const contact = (await import(`../../i18n/pages/contact/${locale}.json`))
+      .default;
+
+    return {
+      ...home,
+      ...privacyPolicy,
+      ...commons,
+      ...news,
+      ...donate,
+      ...contact,
+    };
+  } catch (error) {
+    notFound();
+  }
 }
