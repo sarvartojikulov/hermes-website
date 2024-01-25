0027237434015dac79d17b619536283ca7523ea0
Sarvar Tojikulov <sarvar3099@gmail.com>
2024-01-25 21:34:44 +0100

    feat: add titles

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,16 +19,23 @@ type Props = {
 
 const inter = Wix_Madefor_Display({ subsets: ["latin", "cyrillic"] });
 
-export const metadata: Metadata = {
-    title: "Hermes",
-    description: "Hermes charity fund",
-};
+export const metadata: Metadata = {};
 
 export default async function RootLayout({
     children,
     params: { locale },
 }: Props) {
+
+    if(locale === "en") {
+        metadata["title"] = "Charity Fund «HERMES»",
+        metadata["description"] = "Join Charity Fund HERMES in providing crucial support to Ukraine's Armed Forces and civilians affected by the war. Learn how you can donate, request assistance, and make a difference today."
+    } else {
+        metadata["title"] = "Благодійний фонд «ГЕРМЕС»",
+        metadata["description"] = "Приєднайтеся до Благодійного фонду ГЕРМЕС у наданні важливої ​​підтримки Збройним Силам України та цивільному населенню, постраждалому від війни. Дізнайтеся, як ви можете пожертвувати, попросити допомоги та змінити ситуацію сьогодні."
+    }
+
     const messages = await getIntlMessages(locale);
+    
     return (
         <html lang="en">
             <body className={inter.className}>
