1af316bac84979bae1354710149583cb59d78ef7
Sarvar Tojikulov <sarvar3099@gmail.com>
2024-01-27 19:22:16 +0100

    feat: add new posts

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -118,7 +118,7 @@ const Blog: React.FC = () => {
                     }}
                     loop={true}
                 >
-                    {[0, 1, 2, 3, 4, 5].map((item, i) => (
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10].map((item, i) => (
                         <SwiperSlide key={item + i}>
                             <Post
                                 post={{
